d10062bad8b6c3e92c4eef93528e012c9916180d
Adrien <adrien.biz@gmail.com>
2022-08-22 22:17:37 +0400

    Feat: password confirmation

--- script/script.js
+++ script/script.js
@@ -1,7 +1,23 @@
 const pwd = document.querySelector('#password');
+const pwdConfirm = document.querySelector('#password-confirmation');
+const pwdMatchDisplay = document.querySelector('#pwd-match-display');
 
-pwd.addEventListener('input', test);
+pwd.addEventListener('input', pwdMatch);
+pwdConfirm.addEventListener('input', pwdMatch);
 
-function test() {
-    console.log('haha');
+function pwdMatch() {
+    console.log(pwd.value);
+    console.log(pwdConfirm.value);
+    if (pwd.value && pwdConfirm.value) {
+        if (pwd.value === pwdConfirm.value) {
+            console.log('works!');
+            pwdMatchDisplay.textContent = 'Passwords match';
+            pwdMatchDisplay.style.color = 'green';
+        } else {
+            pwdMatchDisplay.textContent = 'Passwords do not match';
+            pwdMatchDisplay.style.color = 'red';
+        }
+    } else {
+        pwdMatchDisplay.textContent = '';
+    }
 }
